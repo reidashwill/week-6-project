e124afe78763d79783e0062986482bddf5a82186
Reid Ashwill <reidashwill@gmail.com>
2020-04-24 15:49:00 -0700

    troubleshoot error messages

--- src/main.js
+++ src/main.js
@@ -25,6 +25,9 @@ $(document).ready(function(){
       conversions.btcPrice = response2.USD.buy;
       console.log(conversions.btcPrice)
       conversions.convert(response);
+      if(jsonifiedResponse.currency !== "eur" || jsonifiedResponse.currency !== "cad" || jsonifiedResponse.currency !== "dkk" || jsonifiedResponse.currency !== "egp" || jsonifiedResponse.currency !== "gbp" || jsonifiedResponse.currency !== "nok" || jsonifiedResponse.currency !== "sek" || jsonifiedResponse.currency !== "rub"){
+        alert("This currency is not currently supported!")
+      }
       $(".results").show();
       $("#input-return").html(conversions.usd);
       $("#converted-return").html(" " + conversions.converted);
