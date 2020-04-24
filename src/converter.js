029974536cd514ede3c9d3bbeb59d688a8ca2953
Reid Ashwill <reidashwill@gmail.com>
2020-04-24 16:11:05 -0700

    troubleshoot using sessionStorage

--- src/converter.js
+++ src/converter.js
@@ -3,19 +3,24 @@ export class Converter{
   async apiCall() {
       let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
       let jsonifiedResponse;
-      if (response.ok && response.status === 200) {
-        jsonifiedResponse = await response.json();
-      }else{
-        jsonifiedResponse = false;
-      }
-      if(jsonifiedResponse.error === "invalid-key"){
-        alert("Please verify that you are using a valid API key");
-      }else if(jsonifiedResponse.error === "unknown-code"){
-        alert("This currency is not available for conversion");
-      }else if(jsonifiedResponse.error === "malformed-request" || jsonifiedResponse.error === "quota-reached"){
-        alert("Something went wrong!  Please reach out to us at: https://github.com/reidashwill/week-6-project/issues");
-      }
-      return jsonifiedResponse;
+      let storedData;
+      if(storedData !== null){
+        if (response.ok && response.status === 200) {
+          jsonifiedResponse = await response.json();
+          sessionStorage.setItem('dataStored', jsonifiedResponse);
+          storedData = sessionStorage.getItem('dataStored');
+        }else{
+          jsonifiedResponse = false;
+        }
+        if(jsonifiedResponse.error === "invalid-key"){
+          alert("Please verify that you are using a valid API key");
+        }else if(jsonifiedResponse.error === "unknown-code"){
+          alert("This currency is not available for conversion");
+        }else if(jsonifiedResponse.error === "malformed-request" || jsonifiedResponse.error === "quota-reached"){
+          alert("Something went wrong!  Please reach out to us at: https://github.com/reidashwill/week-6-project/issues");
+        }
+        return jsonifiedResponse;
+      }  
   }
   async bitCoinApiCall() {
     let response2 = await fetch(`https://blockchain.info/ticker`);
