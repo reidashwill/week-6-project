6b02593f7d4ab700ee3bce3ddd15076f9c70b2b8
Reid Ashwill <reidashwill@gmail.com>
2020-04-24 08:25:22 -0700

    add an API call in converter.js and call it from inside the document ready function in main.js

---
+++ src/converter.js
@@ -0,0 +1,20 @@
+export class Converter{
+
+  async apiCall() {
+    try{
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      }else{
+        
+        jsonifiedResponse = false;
+      }
+      console.log(jsonifiedResponse);
+      return jsonifiedResponse;
+    }catch(error){
+      return false;
+    }
+    
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { Converter } from './../src/converter.js';
+
+$(document).ready(function(){
+  let conversionTest = new Converter
+  conversionTest.apiCall();
+})
